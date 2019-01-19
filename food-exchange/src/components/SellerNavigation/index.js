1de7322b9b32d78a887d4282f186f2450c825405
Calvin Tantio <calvin.tantio@outlook.com>
2019-01-20 05:19:46 +0800

    ok

--- food-exchange/src/components/SellerNavigation/index.js
+++ food-exchange/src/components/SellerNavigation/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 import * as ROUTES from '../../constants/routes';
 
 const SellerNavigation = () => (
-  <header>
-    <nav>
-      <ul>
-      <li><Link to={ROUTES.HOME}>Home</Link></li>
-        <li><Link to={ROUTES.SELLER_SIGN_IN}>Sign In</Link></li>
-        <li><Link to={ROUTES.SELLER_TRANSACTIONS}>Transactions</Link></li>
-        <li><Link to={ROUTES.SELLER_CALENDAR}>Calendar</Link></li>
-      </ul>
-    </nav>
-  </header>
+  <nav class="navbar navbar-expand-lg navbar-light navbar-light">
+  <a class="navbar-brand">FoodExchange</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link"><Link to={ROUTES.SELLER_CALENDAR}>Calendar</Link></a>
+      <a class="nav-item nav-link"><Link to={ROUTES.SELLER_TRANSACTIONS}>Transactions</Link></a>
+      <a class="nav-item nav-link"><Link to={ROUTES.HOME}>Sign Out</Link></a>
+    </div>
+  </div>
+</nav>
 )
 
 export default SellerNavigation;
