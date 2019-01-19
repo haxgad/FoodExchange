6f5436e0c097c15a5c1850a37d4d6c473d34ec8d
Calvin Tantio <calvin.tantio@outlook.com>
2019-01-19 17:37:30 +0800

    Bootstrap

--- food-exchange/src/components/BuyerHome/index.js
+++ food-exchange/src/components/BuyerHome/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const BuyerHome = () => (
-  <div>
-    <h1>BuyerHome</h1>
+  <div className="jumbotron">
+    <h1 className="display-4">Hello, world!</h1>
+    <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <p className="lead">
+      <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
   </div>
 );
 
--- food-exchange/src/index.js
+++ food-exchange/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Landing from '.components/Landing';
+import Landing from './components/Landing';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Landing />, document.getElementById('root'));
+import BuyerHome from './components/BuyerHome'
+
+// ReactDOM.render(<Landing />, document.getElementById('root'));
+ReactDOM.render(<BuyerHome />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
