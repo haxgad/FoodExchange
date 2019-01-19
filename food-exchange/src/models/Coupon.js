23dbab54fdb774096bd2d9aa0581d50a66109e9f
Calvin Tantio <calvin.tantio@outlook.com>
2019-01-19 20:53:22 +0800

    BuyerHome done

--- food-exchange/src/models/Coupon.js
+++
@@ -1,9 +0,0 @@
-export default class Coupon {
-    constructor(date, time, amount, location, mealType, telegramHandle) {
-      this.date = date
-      this.time = time
-      this.amount = amount
-      this.location = mealType
-      this.telegramHandle = telegramHandle
-    }
-  }
