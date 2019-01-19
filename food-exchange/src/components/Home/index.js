827370f4c8a73b6f8fbc40698cd691daae6a59b7
jacqcjn <jacquelinecjn@gmail.com>
2019-01-19 19:00:09 +0800

    added buttons to home

--- food-exchange/src/components/Home/index.js
+++ food-exchange/src/components/Home/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './home.css';
 
 const Home = () => (
-  <div>
-    <h1>Home</h1>
+  <div className="jumbotron">
+    <div class="container">
+      <h1 className="display-3 welcome">Welcome</h1>
+      <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+      <div class="row">
+        <div class="col-xs-6">
+          <button type="button" class="btn seller btn-md"><a href ="/sellerhome">Seller</a></button>
+        </div>
+        <div class="col-xs-6">
+          <button type="button" class="btn buyer"><a href ="/buyerhome">Buyer</a></button>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
